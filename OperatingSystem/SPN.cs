f0ce5bc6774005febe2cbe068210506c6e090a40
WeGeon <wigon99@gmail.com>
2022-05-02 23:47:05 +0900

    FIX SPN ERROR
    Form1.cs 및 SPN 오류 수정

--- OperatingSystem/SPN.cs
+++ OperatingSystem/SPN.cs
@@ -27,10 +27,12 @@ namespace OperatingSystem
             {
                 /* At(도착시간)이 된 프로세스들 레디큐에 삽입 반복 */
                 if (processList[i].At == Form1.time)
+                {
                     readyQueue.Add(processList[i]);
-                readyQueue = readyQueue.OrderBy(x => x.Bt).ToList();
-
+                }
             }
+
+            List<Process> readyQueueSort = readyQueue.OrderBy(x => x.Bt).ToList();
 
             // 3-2) 실행
             for (int i = 0; i < processorList.Length; i++)
@@ -51,8 +53,9 @@ namespace OperatingSystem
                         if (readyQueue.Count != 0)          // 레디큐에 프로세스 존재
                         {
                             processorList[i].addProcess(readyQueue[0]);     // BT순으로 오름차순 정렬된 레디큐의 맨 앞 프로세스 할당
-                            readyQueue.RemoveAt(0);         // 레디큐에서 제외
-                            readyQueue.RemoveAt(processList.IndexOf(readyQueue[0]));
+                            int idx = processList.IndexOf(readyQueueSort[0]);
+                            readyQueue.RemoveAt(readyQueue.IndexOf(readyQueueSort[0]));
+                            readyQueueSort.RemoveAt(0);         // 레디큐에서 제외
                             processorList[i].setRunning(true);  // 프로세서 실행 상태 실행중으로 변경
                         }
 
@@ -65,11 +68,11 @@ namespace OperatingSystem
                 // 3-2-2) 해당 프로세서가 비어있을 때
                 else
                 {
-                    if (readyQueue.Count != 0)
+                    if (readyQueueSort.Count != 0)
                     {
-                        processorList[i].addProcess(readyQueue[0]);
-                        readyQueue.RemoveAt(0);
-                        readyQueue.RemoveAt(processList.IndexOf(readyQueue[0]));
+                        processorList[i].addProcess(readyQueueSort[0]);
+                        readyQueue.RemoveAt(readyQueue.IndexOf(readyQueueSort[0]));
+                        readyQueueSort.RemoveAt(0);
                         processorList[i].runningTime += 1;
                         processorList[i].setRunning(true);
                     }
